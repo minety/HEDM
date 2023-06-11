d998509fd625d81c76d2ae57fafe45ec25a4add0
Ye Tian <yetian@tians-mbp-14.acasmart.jh.edu>
2023-06-11 00:21:28 -0400

    update

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 ### setup.py
 
-```python
 from setuptools import setup, find_packages
 
 with open('README.md', 'r', encoding='utf-8') as f:
@@ -8,33 +7,25 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='hedm_pre',
-    version='0.0.1',
+    version='0.0.4',
     author='Ye Tian',
     author_email='ytian37@jhu.edu',
+    scripts=['HEDM_Pre.py'],
     description='Tools for pre-processing data for High Energy Diffraction Microscopy (HEDM) analysis',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/minety/hedm_pre',
+    url='https://github.com/minety/HEDM',
     packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD 3-Clause',
+        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
     python_requires='>=3.8',
     install_requires=[
-        'numpy',
-        'hexrd',
-        'h5py',
-        'tifffile',
-        'PyYAML',
-        'os',
-        'h5py',
-        're',
-        'subprocess',
-        'sys'
+        'numpy'
     ]
 )

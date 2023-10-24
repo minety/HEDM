ebe905144e3ef1ffeb5ef37ebd4deeb0a9f99af3
Ye Tian <yetian@tians-mbp-14.acasmart.jh.edu>
2023-10-24 11:34:53 -0400

    update

--- HEDM_Platform/setup.py
+++ HEDM_Platform/setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='HEDM_Platform',
-    version='0.2.1',
+    version='0.2.2',
     author='Ye Tian',
     author_email='ytian6688@hotmail.com, ytian37@jhu.edu',
     entry_points={
